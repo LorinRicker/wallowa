d378026c367c4b2e5f2b7f334939bac13d3e8885
Lorin Ricker <Lorin@RickerNet.us>
2014-04-23 20:06:52 -0600

    saving this fragment from autocat.rb

---
+++ composers.rb
@@ -0,0 +1,58 @@
+composers = {  # Just a starter set -- edit the .yaml file for future updates:
+  "Alka"  => "Charles Valentin Alkan",
+  "JSB"   => "Johann Sebastian Bach",
+  "LvB"   => "Ludwig von Beethoven",
+  "JBr"   => "Johannes Brahms",
+  "BB"    => "Bela Bartók",
+  "Buso"  => "Ferruccio Busoni",
+  "Cart"  => "Elliot Carter",
+  "Copl"  => "Aaron Copland",
+  "Fein"  => "Samuil Feinberg",
+  "Gers"  => "George Gershwin",
+  "Godo"  => "Leopold Godowsky",
+  "Grai"  => "Percy Grainger",
+  "Hind"  => "Paul Hindemith",
+  "Ives"  => "Charles Ives",
+  "Kapu"  => "Nikolai Kapustin",
+  "Lisz"  => "Franz Liszt",
+  "Mart"  => "Bohuslav Martinů",
+  "Medt"  => "Nikolai Medtner",
+  "Mend"  => "Felix Mendelssohn",
+  "Nanc"  => "Conlon Nancarrow",
+  "Orns"  => "Leo Ornstein",
+  "Pers"  => "Vincent Persichetti",
+  "Prok"  => "Sergei Prokofiev",
+  "Rach"  => "Sergei Rachmaninov",
+  "Rzew"  => "Frederic Rzewski",
+  "Shos"  => "Dmitri Shostakovich",
+  "Sora"  => "Kaikhosru Shapurji Sorabji",
+  "Stra"  => "Igor Stravinsky",
+  "Stev"  => "Ronald Stevenson",
+  "Vill"  => "Heitor Villa-Lobos",
+  "Vine"  => "Carl Vine"
+  }
+
+pianists = {
+  "Mart"  => "Martha Argerich",
+  "Malc"  => "Malcolm Bilson",
+  "Dave"  => "Dave Brubeck",
+  "Gerg"  => "Georges Cziffra",
+  "Erro"  => "Errol Garner",
+  "Rich"  => "Richard Goode",
+  "Glen"  => "Glenn Gould",
+  "Vlad"  => "Vladimir Horowitz",
+  "Marc"  => "Marc-André Hamelin",
+  "Step"  => "Stephen Hough",
+  "Olga"  => "Olga Kern",
+  "John"  => "John Ogdon",
+  "Osca"  => "Oscar Peterson",
+  "Andr"  => "Andre Previn",
+  "Svia"  => "Sviatoslav Richter",
+  "Char"  => "Charles Rosen",
+  "Howa"  => "Howard Shelley"
+  }
+
+# composers.merge!( configuration( MUSICIANFILE, composers ) )
+# save = options[:save]
+# options[:save] = false  # don't ever write true for this flag!...
+# options.merge!( configuration( CONFIGFILE, options, save ) )
