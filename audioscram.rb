b3ce93f37f2506ca256312c2e8874f1344554a62
Lorin Ricker <Lorin@RickerNet.us>
2014-07-08 16:15:11 -0600

    version 0.2, working out some ideas

--- audioscram.rb
+++ audioscram.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
-# audioscram.rb
+# audioscram.rb ("Audio Scramble")
 #
 # Copyright © 2014 Lorin Ricker <Lorin@RickerNet.us>
 # Version info: see PROGID below...
@@ -21,10 +21,11 @@
 #           means that I hear the most recently loaded music first (if I
 #           select "All Music" to play from "the beginning").
 #
-#           But I have an excellent memory for not only the music itself, but
-#           also for the order in which it's played.  Hence, if I start hearing
+#           I have an great memory for not only the music itself, but also
+#           for the order in which it's played.  Hence, if I start hearing
 #           the same works played in the same order, "That ain't right..."
-#           occurs to me immediately.
+#           occurs to me immediately.  I hate listening to the same old
+#           program cycle over and over again.  But how to mix it up?
 #
 # Solution: * I need to be able to occasionally reload the Player "from scratch"
 #           * and in a randomized load-order, so that it will build a new and
@@ -54,10 +55,10 @@
 
 # ===========
 # Command Line --
-#   $ audioscram [options] player-device-subtree [ source-subtree ]
+#   $ audioscram [options] /media/player/path/ [ ./RipLibrary/path ]
 #
-#     player-device-subtree is the first argument so that source-subtree can
-#     be defaulted to current working directory, as desired.
+#     /media/player/path/ is the first argument so that ./RipLibrary/path
+#     can be defaulted to current working directory, as desired.
 
 # Process/Algorithm --
 #
@@ -86,11 +87,11 @@
 #    b) Based on com-line options, any Player file-removals (erasures)
 #       are done.
 #
-#    c) Using Dir.glob on the source-subtree, MP3 (or Ogg) files are paired
-#       with a unique random number (integer) from the range 1..UL (and any
-#       collisions are retried until a unique random number is obtained).
-#       This pair is tucked into a hash as Uni-Rand-Int -> MP3-file.  Yes,
-#       this will become a large hash.
+#    c) Using Dir.glob on the ./RipLibrary/path, MP3 (or Ogg) files are
+#       paired with a unique random number (integer) from the range 1..UL
+#       (and any collisions are retried until a unique random number is
+#       obtained).  This pair is tucked into a hash as Rand-Int => MP3-file.
+#       Yes, this will become a large hash.
 #
 #    d) When all music files have been paired and tucked into the hash, the
 #       hash is sorted, resulting in a (large) sorted array of array-pairs.
@@ -107,7 +108,7 @@
 
 
 PROGNAME = File.basename $0
-  PROGID = "#{PROGNAME} v1.00 (05/29/2014)"
+  PROGID = "#{PROGNAME} v1.00 (06/02/2014)"
   AUTHOR = "Lorin Ricker, Castle Rock, Colorado, USA"
 
    CONFIGDIR = File.join( ENV['HOME'], ".config", "#{PROGNAME}" )
@@ -123,22 +124,122 @@ require_relative 'FileEnhancements'
 
 # ==========
 
+options = {  # hash for all com-line options:
+  :keep    => false,
+  :help    => false,
+  :about   => false,
+  :dryrun  => false,
+  :debug   => false,
+  :verbose => false
+  }
 
-# OptionParse
-# remove files from player-device-subtree
-# SCALEFACTOR = 10000
-# fcnt = Dir.glob( "./RipLibrary/**/*.mp3" ).count
-# UL = fcnt * SCALEFACTOR
-# randy = Random.new( Random.new_seed )
-# rno = randy.rand( 1..UL )
-# fileshash = Hash.new
-# Dir.glob( "./RipLibrary/**/*.mp3" ).each do |f|
-#   rno = randy.rand( 1..UL )
-#   fileshash << rno => f
-#   end
+optparse = OptionParser.new { |opts|
+  # --- Program-Specific options ---
+  opts.on( "-k", "--keep", "keep (do not erase) existing player repertory" ) do |val|
+    options[:keep] = true
+  end  # -k --keep
+  opts.on( "-r", "--refresh", "refresh each player repertory file" ) do |val|
+    options[:refresh] = true
+  end  # -r --refresh
+  # --- Set the banner & Help option ---
+  opts.banner = "Usage: #{PROGNAME} [options] /media/player/path ./RipLibrary/path"
+  opts.on( "-?", "-h", "--help", "Display this help text" ) do |val|
+    puts opts
+    exit true
+  end  # -? --help
+  # --- About option ---
+  opts.on( "-a", "--about", "Display program info" ) do |val|
+    puts "#{PROGID}"
+    puts "#{AUTHOR}"
+    exit true
+  end  # -a --about
+  # --- DryRun option ---
+  opts.on( "-n", "--dryrun", "Dry run: do not actually modify files or environment" ) do |val|
+    options[:dryrun] = true
+  end  # -n --dryrun
+  # --- Debug option ---
+  opts.on( "-d", "--debug", "Debug mode (more output than verbose)" ) do |val|
+    options[:debug] = true
+  end  # -d --debug
+  # --- Verbose option ---
+  opts.on( "-v", "--verbose", "Verbose mode" ) do |val|
+    options[:verbose] = true
+  end  # -v --verbose
+}.parse!  # leave residue-args in ARGV
+
+# Some options override:
+options[:keep] = true if options[:refresh]
+
+# Configure FileUtils options from audioscram com-line options:
+fopts = Hash.new
+fopts[:verbose] = options[:verbose] || options[:dryrun]
+fopts[:noop]    = options[:dryrun]
+
+# TO-DO
+# Player mounts as /dev/sdb ("/media/lorin/SANSA CLIPP"), type vfat
+#           and as /dev/sdc1 ("/media/lorin/4219-1B1E"), type vfat
 #
-# filesarray = fileshash.sort
-# filesarray.each do |pair|
-#   copy pair[1] to player-device-subtree
-#   end
+# Reformat with new labels POPMUSIC and CLSMUSIC (still type vfat)
+#
+# Be sure that Player's Settings | System Settings | USB Mode
+# is set to MCS (not MTP or Auto Detect)...
+#
 
+# Get player-path from 1st com-line arg, expand it if necessary:
+player = File.dirname( ARGV[0] )
+player = Dir.pwd if player == '.'
+puts "player is '#{player}'" if options[:verbose]
+
+# Get source-path from 2st com-line arg, expand it if necessary:
+source = File.dirname( ARGV[1] )
+source = Dir.pwd if source == '.'
+puts "source is '#{source}'" if options[:verbose]
+
+# Count music files in this source-path:
+MFSCALE = 10000
+mp3spec = File.join( source, "**", "*.mp3" )
+cnt = Dir.glob( mp3spec ).count
+uplim = cnt * MFSCALE
+puts "Music file count: #{cnt}, upper limit: #{uplim}" if options[:verbose]
+
+# Randomize -- pair each music file with a random number:
+randgen = Random.new( Random.new_seed )
+scramhash = Hash.new
+maxcolcnt = 0
+Dir.glob( mp3spec ).each do | mf |
+  # Search for an unpaired random number (usually hits the first time):
+  colcnt = 0
+  until ! scramhash[ rno = randgen.rand( 1..uplim ) ]
+    colcnt += 1  # Aha! A random key collision!...
+  end
+  scramhash[rno] = mf
+  maxcolcnt = colcnt if maxcolcnt < colcnt   # Track maximum # of random hash key collisions
+end  # Dir.glob
+## pp scramhash if options[:verbose]
+msg = "Maximum random hash key collisions: #{maxcolcnt}"
+puts msg if options[:verbose] && maxcolcnt > 0
+
+# Randomized -- sort the music files by random# -- a hash sort
+#   produces a nested array: [[k1,v1],[k2,v2],[k3,v3]...]:
+scramarry = scramhash.sort
+scramarry.each { |e| puts "#{e[0]}: #{File.basename( e[1] )}" } if options[:verbose]
+
+# TO-DO:
+# (X) OptionParse
+# ( ) if !options[:keep], remove files from /media/player/path/
+# (X) SCALEFACTOR = 10000
+# (X) cnt = Dir.glob( "./RipLibrary/**/*.mp3" ).count
+# (X) uplim = cnt * SCALEFACTOR
+# (X) randgen = Random.new( Random.new_seed )
+# (X) scramhash = Hash.new
+# (X) Dir.glob( "./RipLibrary/**/*.mp3" ).each do |f|
+#      rno = randgen.rand( 1..uplim )
+#      scramhash[rno] = mf
+#      end
+#
+# (X) scramarry = scramhash.sort
+# ( ) scramarry.each do |pair|
+# ( )   if options[:refresh], copy MP3 (overwrite existing),
+#       else copy MP3 only if it does not exist in Player
+#       copy pair[1] to /media/player/path/: cp( "src"... "dst" )
+#       end
