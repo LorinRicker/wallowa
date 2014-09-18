ef47ffbfe278cede3f88c01ab60645e4da05b8a1
Lorin Ricker <Lorin@RickerNet.us>
2014-09-17 20:20:05 -0600

    first fully usable version; minor problems to track down

--- lib/Scramble.rb
+++ lib/Scramble.rb
@@ -4,7 +4,7 @@
 # Scramble.rb
 #
 # Copyright © 2014 Lorin Ricker <Lorin@RickerNet.us>
-# Version 1.0, 09/15/2014
+# Version 1.0, 09/17/2014
 #
 # This program is free software, under the terms and conditions of the
 # GNU General Public License published by the Free Software Foundation.
@@ -15,16 +15,21 @@ require 'pp'
 # Dependency: Uses Ruby *Random* standard library module (available since v2.x).
 #
 # Provides the means to shuffle, scramble or thoroughly mix-up a collection of
-# arbitrary objects, to randomly retrieve (e.g., "deal") a collection which is
-# read in a predictable order.  The physical analogy is to "shuffle a deck of
-# playing cards" and then to "deal" the cards one-at-a-time, or to "shake up a
-# bag of marbles" and then to choose the marbles one-at-a-time.
+# arbitrary objects, and to randomly retrieve (e.g., "deal") a subset of one or
+# more of those objects.
 #
-# Relies on two instance variables:
+# The physical analogy is to "shuffle a deck of playing cards" and then to
+# "deal" the cards one-at-a-time, or to "shake up a bag of marbles" and then
+# to choose the marbles one-at-a-time.
+#
+# Relies on three instance variables:
+#   @vals : An array which holds the 'raw' objects (in the order stored)
 #   @pile : A hash which collects the objects, associating each stored object
 #           (the Value) with a random number (its Key)
 #   @deck : An array which receives a sorted version of the @pile hash, thus
 #           presenting its elements as a sorted list of [rand#,object] pairs
+#           retrieval of these array elements "in sorted order" is now equi-
+#           valent to "dealing them in random order".
 #
 # Because, as each Value object is @pile-stored, it is associated with a unique
 # (and potentially different for each run) random number, and then the @pile is
@@ -35,6 +40,7 @@ require 'pp'
 #
 # Instance variables:
 #
+#    @vals       : An array which holds the 'current' values (for reshuffles)
 #    @pile       : A hash which collects the (unrandomized) collection objects
 #    @deck       : An array which is the randomized collection
 #    @deckindex  : Initially nil to indicate that objects are being collected
@@ -48,62 +54,61 @@ require 'pp'
 #                  fixed seed is not provided to Scramble.new, then each instance
 #                  of Scramble runs with a unique (pseudo)Random number sequence;
 #                  a specific, shared seed value puts each instance on the same
-#                  (pseudo)Random sequence.
-#    @collisions : An internal bookkeeping counter, just to see if random numbers
-#                  reoccur which would cause hash table (key) collisions.  Reported
-#                  only by to_s, and just for information.
+#                  (pseudo)Random sequence/cycle.
 #
-# For convenience, +@pile+, +@deck+, +@deckindex+, +@seed+ and +@collisions+ are
-# exposed as read-only attributes of the class.
+# For convenience, +@pile+, +@deck+, @vals, +@deckindex+ and +@seed+ are exposed
+# as read-only attributes of the class.
 
 class Scramble
 
   class EndOfCollection < StandardError; end
   class NotYetDone      < StandardError; end
 
-  attr_reader :pile, :deck
-  attr_reader :deckindex, :seed, :collisions
+  attr_reader :vals, :pile, :deck
+  attr_reader :deckindex, :seed
 
   # Responds to <tt>obj = Scramble.new( seed )</tt>.
   def initialize( seed = nil )
-    @collisions = 0
-    @pile = Hash.new
+    reset_rand( seed )
     self.reset
-    self.reset_rand( seed )
+    @factorial_series = [ 1 ]
   end  # initialize
 
-  # Resets (starts over) with a new +@deck+.  +@deckindex+ is (re)set to +nil+
-  # indicating that the +@deck+ is empty (new) and unsorted. Calling (client)
-  # environment must now (re)+store+ objects into the +@pile+; when the last
-  # object has been stored in the +@pile+, the calling environment must invoke
-  # +store_done+ to "randomize" (sort) the +@pile+ into the +@deck+.
+  # Resets (starts over) with a new collection of +@vals+.
+  #
+  # +@deckindex+ is (re)set to +nil+ indicating that the +@deck+
+  # is empty (new) and unsorted.
+  #
+  # Calling (client) environment must now (re)+store+ objects into the +@pile+;
+  # when the last object has been stored in the +@pile+, the calling environment
+  # must invoke +shuffle+ to "randomize" (sort) the +@pile+ into the +@deck+.
   def reset
-    @deck = Array.new
-    @deckindex = 0
+    @vals = []
+    reset_deck
   end  # reset
 
-  # Stashes (stores) the parameter object in the +@pile+ hash, assigning
-  # it a random key, such that when +@pile+ is later sorted into +@deck+,
-  # the result is a "randomized sequence" when retrieved sequentially from
-  # that +@deck+ array.  Ensures that +@deckindex+ remains +nil+ until
-  # +store_done+ is invoked to "randomize" (sort) +@pile+ into the +@deck+,
-  # at which point calling environment can invoke +deal+ and/or +fetch+ to
-  # retrieve +@deck+ objects.
+  # Stashes (stores) the parameter object in the +@vals+ array.
+  #
+  # Ensures that +@deckindex+ remains +nil+ until +shuffle+ is invoked to
+  # "randomize" (sort) +@pile+ into the +@deck+, at which point calling
+  # environment can invoke +deal+ and/or +fetch+ to retrieve +@deck+ objects.
   def store( obj )
-    until ! @pile[ rno = @randgen.rand ]
-      @collisions += 1
-    end
-    @pile[rno] = obj
-    @deckindex = nil
+    @vals << obj
+    #@pile[ @randgen.rand ] = obj
+    @deckindex = nil if not @deckindex
+    return @vals.size
   end  # store
 
-  # Prepares a new +@deck+, (re)sets +@deckindex+ from +nil+ (falsity) to +0+,
-  # ready to fetch first object from +@deck+, and assigns the "randomized" (sorted)
-  # +@pile+ to +@deck+.
-  def store_done
-    self.reset
+  # Stashes (stores) each element of @vals into the +@pile+ hash, assigning
+  # it a random key, and then sorts +@pile+ into +@deck+.  The result is a
+  # "randomized sequence" when retrieved sequentially from +@deck+.
+  def shuffle
+    @pile = Hash.new
+    @vals.each { |v| @pile[ @randgen.rand ] = v }
     @deck = @pile.sort
-  end  # store_done
+    @deckindex = 0   # ready to deal...
+    return @deck.size
+  end  # shuffle
 
   # Returns "the next" object to caller from +@deck+, keeping internal track of
   # "next object" via +@deckindex+, or returns +nil+ when the last legitimate
@@ -118,17 +123,18 @@ class Scramble
   #
   # Both +fetch+ and +deal+ require that the calling environment has stored one
   # or more objects via +store+, and when all desired objects have been stored
-  # in the +@pile+, the caller has invoked +store_done+ to "randomize" (sort)
+  # in the +@pile+, the caller has invoked +shuffle+ to "randomize" (sort)
   # the +@pile+ into the +@deck+.  Any attempt to +fetch+ *after* +reset+ and
-  # *before* +store_done+ have been called results in raising the +NoteYetDone+
+  # *before* +shuffle+ have been called results in raising the +NotYetDone+
   # exception.
   def fetch( valueonly = true )
     if @deckindex
-      @deckindex += 1
       if @deckindex < @deck.length
-        valueonly ? @deck[@deckindex][1] : @deck[@deckindex]
+        val = valueonly ? @deck[@deckindex][1] : @deck[@deckindex]
+        @deckindex += 1
+        return val
       else
-        self.reset
+        @deckindex = 0  # recycle again...
         return nil
       end
     else
@@ -144,38 +150,95 @@ class Scramble
   #
   # Both +deal+ and +fetch+ require that the calling environment has stored one
   # or more objects via +store+, and when all desired objects have been stored
-  # in the +@pile+, the caller has invoked +store_done+ to "randomize" (sort)
+  # in the +@pile+, the caller has invoked +shuffle+ to "randomize" (sort)
   # the +@pile+ into the +@deck+.  Any attempt to +fetch+ *after* +reset+ and
-  # *before* +store_done+ have been called results in raising the +NoteYetDone+
+  # *before* +shuffle+ have been called results in raising the +NoteYetDone+
   # exception.
- def deal( numobj )
+ def deal( numobj = 1, valueonly = true )
     hand = []
     exhausted = false
     until hand.length >= numobj || exhausted
-      val = self.fetch
+      val = self.fetch( valueonly )
       exhausted = (val == nil)
-      hand << val if !exhausted
+      hand << val if not exhausted
     end
     hand << nil while hand.length < numobj
-    return hand
+    return hand, exhausted
   end  # deal
 
-  # Custom <b>to string</b> dumps the class's instance variables
-  # in pretty-print format.
+  # Calculates n! (n-factorial), using a memoized (@factorial_series) algorithm.
+  def factorial( n )
+    @factorial_series[n] ||= n * factorial( n-1 )
+  end  # factorial
+  # +n!+ is an alias for this class's +factorial+ method.
+  alias :n! :factorial
+
+  # Calculates the number of combinations of "'n' things taken 'k' at a time",
+  # or the number of k-combinations a set S of n elements.  Wikipedia gives
+  # this calculation as: n!/k!(n-k)!
+  # (see http://en.wikipedia.org/wiki/Combination)
+  def combinations( n, k )
+    return 0 if k > n
+    n!(n) / ( n!(k) * n!(n-k) )
+  end  # combinations
+
+  # Reports the k-combinations of @vals.size elements taken k at a time.
+  def to_combinations( k = 2 )
+    combos = combinations( @vals.size, k )
+  end  # to_combinations
+
+  # Reports the class's instance variables in pretty-print format.
   def to_s
-    puts "size of @pile = #{@pile.length}"
+    puts "size of @vals = #{@vals ? @vals.size : 0 }"
+    pp @vals
+    puts "size of @pile = #{@pile ? @pile.size : 0 }"
     pp @pile
-    puts "size of @deck = #{@deck.length}"
+    puts "size of @deck = #{@deck ? @deck.size : 0 }"
     pp @deck
-    puts "     @deckindex: '#{@deckindex}'"
-    puts "          @seed: '#{@seed}'"
-    puts "    @collisions: '#{@collisions}'"
+    puts "    @deckindex: '#{@deckindex ? @deckindex : 'nil'}'"
+    puts "         @seed: '#{@seed}'"
   end  # to_s
 
-  private
+  private  # =======================
+  def reset_deck
+    @deck = []
+    @deckindex = nil
+  end  # reset_deck
+
   def reset_rand( seed )
     @seed = seed || Random.new_seed
     @randgen = Random.new( @seed )
   end  # reset_rand
 
 end  # class
+
+# ---
+# === Testing / Demo ===
+# use:
+#     $ ruby lib/Scramble.rb
+#
+if $0 == __FILE__
+  require_relative 'StringEnhancements'
+
+  carddeck = Scramble.new
+  cards    = %w[ 1 2 3 4 5 6 7 8 9 J Q K A ]
+  cards.each { |c| carddeck.store( "♠ #{c}")}
+  cards.each { |c| carddeck.store( "♣ #{c}")}
+  cards.each { |c| carddeck.store( "♥ #{c}")}
+  cards.each { |c| carddeck.store( "♦ #{c}")}
+
+  carddeck.shuffle
+  carddeck.to_s
+  tsiz = carddeck.deck.size
+  n = 5
+  puts "\nDeal poker hand (#{tsiz})"
+  (1..6).each { |t| puts "  #{carddeck.deal(n)}" }
+  puts "There are #{carddeck.to_combinations(n).thousands} combinations of #{tsiz} cards dealt #{n} at a time."
+  puts "(This value is verified in http://en.wikipedia.org/wiki/Combination, 'Example of counting combinations')"
+
+  carddeck.shuffle
+  n = 10
+  puts "\nDeal bridge hand (#{tsiz})"
+  (1..4).each { |t| puts "  #{carddeck.deal(n)}" }
+  puts "There are #{carddeck.to_combinations(n).thousands} combinations of #{tsiz} cards dealt #{n} at a time."
+end
