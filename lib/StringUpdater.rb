c7ab09ee0436aac26ff008d8e0b314874976ceed
Lorin Ricker <Lorin@RickerNet.us>
2015-02-13 16:20:25 -0700

    working version (needs more testing): updateCopyright(), for a file-processing loop

--- lib/StringUpdater.rb
+++ lib/StringUpdater.rb
@@ -15,36 +15,36 @@
 
 # -----
 
-module String
+class String
 
   # Check the input string for a canonical 'Copyright © XXXX[-YYYY]' notice,
   # and if found, check that it is current, fix it if its stale.
   # Use:  textline.updateCopyright( [updtoyear: '2014'] [, verbose: true] )
-  def self.updateCopyright( updtoyear: nil, verbose: false )
-    line = self
+  def updateCopyright( updtoyear: nil, verbose: false )
+    line = self.to_s
     unless updtoyear
       require 'date'
-      updtoyear = Date.now.year.to_s
+      updtoyear = DateTime.now.year.to_s
     end
-    decades = '20|19|21'
-    copypat = /Copyright( ©)? (((#{decades})\d\d)(-(#{decades}\d\d)?)?)\s/
+    decades = '19|20|21'
+    copypat = /Copyright( ©)? (((#{decades})\d\d)(-((#{decades})\d\d)?)?)\s/
     # Interesting match groups in this cpat regexp:
     #   #3: always the first/beginning year XXXX
     #   #5: if present, the dash-range and year YYYY
     #   #6: if present, the last/ending year YYYY
     matched = copypat.match( line )
-    return line unless matched          # no match?  done... return unaltered line
+    return line unless matched      # no match?  done... return unaltered line
     if matched[6]
-      if matched[6] == updtoyear
+      if matched[6] >= updtoyear    # current? or also accept future date...
         $stderr.puts "%upd©-current: #{line}" if verbose
         return line
       end
     end
-    #       Copyright ©     XXXX          -    YYYY
-    line = 'Copyright © ' + matched[3] + '-' + matched[6] + ' '
+    #       Copyright © XXXX         -YYYY
+    line = "Copyright © #{matched[3]}-#{updtoyear} "
     line = matched.pre_match + line + matched.post_match
     $stderr.puts "%upd@-updated: #{line}" if verbose
     return line
   end  # updateCopyright
 
-end  # module
+end  # class
