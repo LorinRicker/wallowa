0b3914d9bb97c844dbcf8ac20f8fd60efb9ea6d1
Lorin Ricker <Lorin@RickerNet.us>
2015-02-16 14:42:14 -0700

    replace '(C)' with '©'

--- how-many-chords-on-piano.rb
+++ how-many-chords-on-piano.rb
@@ -3,7 +3,7 @@
 
 # how-many-chords-on-piano.rb
 #
-# Copyright (C) 2012-2015 Lorin Ricker <lorin@rickernet.us>
+# Copyright © 2012-2015 Lorin Ricker <lorin@rickernet.us>
 # Version: 1.1, 01/25/2015
 #
 # This program is free software, under the terms and conditions of the
