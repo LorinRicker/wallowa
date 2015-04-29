756aa26cb30e87bdb71d38428519ad5fb6e69b25
Lorin Ricker <Lorin@RickerNet.us>
2015-04-29 14:51:29 -0600

    v1.0 - a generalized multi-line error message puts: putmsg

---
+++ lib/ErrorMsg.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+
+# ErrorMsg.rb
+#
+# Copyright © 2015 Lorin Ricker <Lorin@RickerNet.us>
+# Version 1.0, 04/29/2015
+#
+# This program is free software, under the terms and conditions of the
+# GNU General Public License published by the Free Software Foundation.
+# See the file 'gpl' distributed within this project directory tree.
+
+# Intelligently formatted error messages --
+
+require_relative '../lib/TermChar'
+
+module ErrorMsg
+
+  SPC = ' '
+
+  def putmsg( msgpreamble, msgtext, msgline2 = '' )
+    msgtext  = SPC + msgtext  if msgtext[0] != SPC
+    msgline2 = SPC + msgline2 if msgline2 != '' && msgline2[0] != SPC
+    msg      =  msgpreamble + msgtext
+    if msgpreamble.size + msgtext.size + msgline2.size < TermChar.terminal_width
+      msg += msgline2
+    else
+      msg += SPC*(msgpreamble.size-1) + msgline2
+    end
+    $stderr.puts msg
+  end  # putmsg
+
+end  # module ErrorMsg
