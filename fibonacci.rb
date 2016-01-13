bdcce7ea2822e29917134dc93aa2e674725ea8c3
Lorin Ricker <Lorin@RickerNet.us>
2016-01-12 18:36:36 -0700

    add com-line ARGV[0] for interactive tests, pretty-printing the results

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,14 +3,14 @@
 
 # fibonacci.rb
 #
-# Copyright © 2012-2015 Lorin Ricker <Lorin@RickerNet.us>
+# Copyright © 2012-2016 Lorin Ricker <Lorin@RickerNet.us>
 # Version 0.4, 06/04/2014
+# Version 0.5, 01/12/2016, adds a com-line ARGV[0] for interactive tests,
+#                          pretty-printing the results.
 #
 # This program is free software, under the terms and conditions of the
 # GNU General Public License published by the Free Software Foundation.
 # See the file 'gpl' distributed within this project directory tree.
-
-require 'pp'
 
 # This recursive algorithm works in exponential time, with O(N**m)
 # costs beginning to be noticeable on fast processors at ~ Fib(32):
@@ -32,3 +32,13 @@ def fibonacci_fast( n )
   @fib_series[n] ||= fibonacci_fast( n-2 ) + fibonacci_fast( n-1 )
 end  # fibonacci_fast
 alias :fib :fibonacci_fast
+
+# Main -- test driver:
+if $0 == __FILE__ then
+  if ARGV[0]  # ain't null
+    require_relative 'lib/ppstrnum'
+    f = fib(ARGV[0].to_i)
+    puts "\nFibonacci_#{ARGV[0]} = #{f.thousands}"
+    puts "\nor #{f.numbernames}"
+  end
+end
