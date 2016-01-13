a3baf5d052079b9f790dee726cbec1d7e9621f8b
Lorin Ricker <Lorin@RickerNet.us>
2016-01-12 18:24:42 -0700

    add com-line ARGV[0] for interactive tests, pretty-printing the results

--- factorial.rb
+++ factorial.rb
@@ -3,18 +3,18 @@
 
 # factorial.rb
 #
-# Copyright © 2011-2015 Lorin Ricker <lorin@rickernet.us>
+# Copyright © 2011-2016 Lorin Ricker <lorin@rickernet.us>
 # Version: 0.5, 06/04/2014
 # Version: 0.6, 06/24/2014, adds Permutation and Combination
 # Version: 0.8, 12/21/2014, adds "Main -- test driver" test-case
 #                           and tests for 52! and 120!
+# Version: 0.9, 01/12/2016, adds a com-line ARGV[0] for interactive tests,
+#                           pretty-printing the results.
 #
 # This program is free software, under the terms and conditions of the
 # GNU General Public License published by the Free Software Foundation.
 # See the file 'gpl' distributed within this project directory tree.
 #
-
-require 'pp'
 
 # This recursive algorithm works in quadratic time:
 def factorial_slow( n )
@@ -53,13 +53,20 @@ alias :comb :combination
 
 # Main -- test driver:
 if $0 == __FILE__ then
-  # The following calculated values for 120! (a 201-digit value) is an
-  #   independent evaluation from https://en.wikipedia.org/wiki/OCaml;
-  #   the value for 52! (a 68-digit value), the number of distinct shuffles
-  #   of 52-cards is from en.wikipedia.org/wiki/Orders_of_magnitude_(numbers)
-  # If these print 'true', then we've got a pretty good cross-check...
-  puts "120! calculation passed" if n!(120).to_s ==
-    "6689502913449127057588118054090372586752746333138029810295671352301633557244962989366874165271984981308157637893214090552534408589408121859898481114389650005964960521256960000000000000000000000000000"
-  puts " 52! calculation passed" if n!(52) ==
-     80658175170943878571660636856403766975289505440883277824000000000000
+  if ARGV[0]  # ain't null
+    require_relative 'lib/ppstrnum'
+    f = n!(ARGV[0].to_i)
+    puts "\n#{ARGV[0]}! = #{f.thousands}"
+    puts "\nor #{f.numbernames}"
+  else
+    # The following calculated values for 120! (a 201-digit value) is an
+    #   independent evaluation from https://en.wikipedia.org/wiki/OCaml;
+    #   the value for 52! (a 68-digit value), the number of distinct shuffles
+    #   of 52-cards is from en.wikipedia.org/wiki/Orders_of_magnitude_(numbers)
+    # If these print 'true', then we've got a pretty good cross-check...
+    puts "120! calculation passed" if n!(120).to_s ==
+      "6689502913449127057588118054090372586752746333138029810295671352301633557244962989366874165271984981308157637893214090552534408589408121859898481114389650005964960521256960000000000000000000000000000"
+    puts " 52! calculation passed" if n!(52) ==
+       80658175170943878571660636856403766975289505440883277824000000000000
+  end
 end
