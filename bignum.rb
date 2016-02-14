274f58dcd24aa6e45285c9fd21472b74e18aa204
Lorin Ricker <Lorin@RickerNet.us>
2016-02-13 23:48:46 -0700

    new utility bignum.rb for exercising Bignum class expressions, with various output\/display options; tweak copyright date on a template file

--- bignum.rb
+++ bignum.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
-# «·».rb
+# bignum.rb
 #
 # Copyright © 2016 Lorin Ricker <lorin@rickernet.us>
 # Version info: see PROGID below...
@@ -12,11 +12,8 @@
 #
 
 PROGNAME = File.basename $0
-  PROGID = "#{PROGNAME} v0.«·» («·»/«·»/2016)"
+  PROGID = "#{PROGNAME} v1.0 (02/13/2016)"
   AUTHOR = "Lorin Ricker, Elbert, Colorado, USA"
-
-   CONFIGDIR = File.join( ENV['HOME'], ".config", PROGNAME )
-  CONFIGFILE = File.join( CONFIGDIR, "#{PROGNAME}.yaml.rc" )
 
 DBGLVL0 = 0
 DBGLVL1 = 1
@@ -26,59 +23,33 @@ DBGLVL3 = 3  # <-- reserved for binding.pry &/or pry-{byebug|nav} #
 # -----
 
 require 'optparse'
-require_relative 'lib/FileEnhancements'  # includes AppConfig class
+require_relative 'lib/ppstrnum'
 
 # ==========
 
-def config_save( opt )
-  # opt is a local copy of options, so we can patch a few
-  # values without disrupting the original/global hash --
-  opt[:about]     = false
-  opt[:debug]     = DBGLVL0
-  opt[:«+»]       = «+»
-  opt[:noop]      = false
-  opt[:sudo]      = ""
-  opt[:update]    = false
-  opt[:verbose]   = false
-  AppConfig.configuration_yaml( CONFIGFILE, opt, true )  # force the save/update
-end  # config_save
-
 # === Main ===
-options = { :«+»     => «+»,
-            :«+»     => «+»,
+options = { :format  => "sep",
             :noop    => false,
-            :sudo    => "",
-            :update  => false,
             :verbose => false,
             :debug   => DBGLVL0,
             :about   => false
           }
 
-options.merge!( AppConfig.configuration_yaml( CONFIGFILE, options ) )
-
 optparse = OptionParser.new { |opts|
-  opts.on( "-«+»", "--«+»",  # "=«+»", String,
-           "«+»" ) do |val|
-    options[:«+»] = «+»
-  end  # -«+» --«+»
+  opts.on( "-f", "--format[=DISPLAY]", /SEP|WORD|BARE|ASC|DESC/i,
+           "Format to display (SEP: comma separated triads,",
+           "  BARE: no separator,",
+           "  WORD: number-names,",
+           "  ASC:  number-names in ascending triads,",
+           "  DESC: number-names in descending triads)" ) do |val|
+    options[:format] = val.downcase || "sep"
+  end  # -f --format
   opts.separator ""
-  opts.separator "    The options below are always saved in the configuration file"
-  opts.separator "    in their 'off' or 'default' state:"
-  opts.on( "-S", "--sudo",
-           "Run this backup/restore with sudo" ) do |val|
-    options[:sudo] = "sudo"
-  end  # -S --sudo
   opts.on( "-n", "--noop", "--dryrun", "--test",
            "Dry-run (test & display, no-op) mode" ) do |val|
     options[:noop]  = true
     options[:verbose] = true  # Dry-run implies verbose...
   end  # -n --noop
-  opts.on( "-u", "--update", "--save",
-           "Update (save) the configuration file; a configuration",
-           "file is automatically created if it doesn't exist:",
-           "#{CONFIGFILE}" ) do |val|
-    options[:update] = true
-  end  # -u --update
   # --- Verbose option ---
   opts.on( "-v", "--verbose", "--log", "Verbose mode" ) do |val|
     options[:verbose] = true
@@ -99,8 +70,9 @@ optparse = OptionParser.new { |opts|
     exit true
   end  # -a --about
   # --- Set the banner & Help option ---
-  opts.banner = "\n  Usage: #{PROGNAME} [options] «+»ARG«+»" +
-                "\n\n   where «+»\n\n"
+  opts.banner = "\n  Usage: #{PROGNAME} [options] EXPRESSION" +
+                "\n\n    where EXPRESSION is a numeric expression to" +
+                "\n    valuate and display in the selected format\n\n"
   opts.on_tail( "-?", "-h", "--help", "Display this help text" ) do |val|
     $stdout.puts opts
     # $stdout.puts "«+»Additional Text«+»"
@@ -116,14 +88,30 @@ if options[:debug] >= DBGLVL3 #
 end                           #
 ###############################
 
-# f1 = ARGV[0] || ""  # a completely empty args will be nil here, ensure "" instead
-# f2 = ARGV[1] || ""
+args = ARGV.join( ' ' )
+bignum = 0
+cmd = "bignum = #{args}"
+puts "cmd: #{cmd}" if options[:verbose]
+eval( cmd )
 
-# ...(optional) other command-line processing goes here...
+case options[:format].to_sym
+when :sep
+    result = bignum.thousands
+when :bare
+    result = bignum
+when :word
+    result = bignum.numbernames
+when :asc
+    result = bignum.numbernames
+                   .split( ',' )
+                   .reverse { |s| trim( s ) }
+                   .join( ",\n")
+when :desc
+    result = bignum.numbernames
+                   .split( ',' ) { |s| trim( s) }
+                   .join( ",\n")
+end
 
-# Update the config-file, at user's request:
-config_save( options ) if options[:update]
-
-# Application processing goes here...
+puts result
 
 exit true
