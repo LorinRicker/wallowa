70b1f7430fbc0acd7aa6cb5e675eb2074e2f214d
Lorin Ricker <Lorin@RickerNet.us>
2016-11-13 19:22:56 -0700

    added optional 3rd parameter returnexit to permit caller to handle the exit itself

--- lib/GetPrompted.rb
+++ lib/GetPrompted.rb
@@ -4,7 +4,7 @@
 # GetPrompted.rb
 #
 # Copyright © 2011-2016 Lorin Ricker <Lorin@RickerNet.us>
-# Version 1.2, 02/03/2015
+# Version 2.0, 11/13/2015
 #
 # This program is free software, under the terms and conditions of the
 # GNU General Public License published by the Free Software Foundation.
@@ -20,12 +20,15 @@ include AppCmdCompletions
 # or returns a +default+ response (string) if available and the user
 # accepts that default by pressing the <Enter> key.
 # Prompt-termination punctuation is a colon ":".
-def getprompted( prompt, default )
+# returnexit set true lets caller receive and handle the exit itself.
+def getprompted( prompt, default, returnexit = false )
   dstr ||= default
   pstr = prompt + ( dstr == "" ? ": " : " [#{dstr}]: " )
   response = readline( pstr, true ).strip
   exit true if response.downcase == "exit" || response.downcase == "quit"
   return ( response != "" ? response : dstr )
 rescue StandardError
+  return false if returnexit  # to let caller handle the exit on its own...
+  # otherwise:
   exit true  # this exit always provides cmd-line status:0
 end #getprompted
