659a2553d05bd1c0891fa7016ba427f02098b0c6
Lorin Ricker <Lorin@RickerNet.us>
2017-06-19 16:36:00 -0600

    add option --variable=VARNAME (-r), help formatting improvements

--- bignum.rb
+++ bignum.rb
@@ -12,7 +12,7 @@
 #
 
 PROGNAME = File.basename $0
-  PROGID = "#{PROGNAME} v2.0 (06/16/2017)"
+  PROGID = "#{PROGNAME} v2.1 (06/19/2017)"
   AUTHOR = "Lorin Ricker, Elbert, Colorado, USA"
 
 DBGLVL0 = 0
@@ -20,6 +20,10 @@ DBGLVL1 = 1
 DBGLVL2 = 2  ######################################################
 DBGLVL3 = 3  # <-- reserved for binding.pry &/or pry-{byebug|nav} #
              ######################################################
+
+DEFAULT_DCLSYMBOL = "BIGNUM_RESULT"
+VMSONLY_BORDER    = "    " + "=== VMS only " + '=' * 70
+VMSONLY_BORDEREND = "    " + '=' * ( VMSONLY_BORDER.length - 4 )
 # -----
 
 require 'optparse'
@@ -36,6 +40,7 @@ options = { :format    => 'sep',
             :separator => ',',
             :indent    => 2,
             :os        => :linux,
+            :dclsymbol => nil,
             :noop      => false,
             :verbose   => false,
             :debug     => DBGLVL0,
@@ -44,27 +49,34 @@ options = { :format    => 'sep',
 
 optparse = OptionParser.new { |opts|
   opts.on( "-f", "--format[=DISPLAY]", /SEP|WORD|BARE|ASC|DESC/i,
-           "Format to display",
-           " (SEP: comma separated triads,",
+           "Format to display:",
+           "  SEP: comma separated triads (default),",
            "  BARE: no separator,",
            "  WORD: number-names,",
            "  ASC:  number-names in ascending triads,",
-           "  DESC: number-names in descending triads)" ) do |val|
+           "  DESC: number-names in descending triads" ) do |val|
     options[:format] = val.downcase || "sep"
   end  # -f --format
   opts.on( "-i", "--indent[=INDENTATION]", Integer,
-           "Display indentation width" ) do |val|
+           "Display indentation width (default is 2 spaces)" ) do |val|
     options[:indent] = val.to_i.abs
   end  # -i --indent
   opts.on( "-j", "--just[=JUSTIFICATION]", /LEFT|RIGHT/i,
-           "Format justification, right (D) or left" ) do |val|
+           "Format justification: right (default) or left" ) do |val|
     options[:just] = val.downcase || "right"
   end  # -j --just
   opts.on( "-s", "--separator[=SEPARATOR]", String,
-           "Triads separator" ) do |val|
+           "Triads separator (default is ',' comma)" ) do |val|
     options[:separator] = val.to_i.abs
   end  # -s --separator
-  opts.separator ""
+  opts.separator "\n#{VMSONLY_BORDER}"
+  opts.on( "-r", "--variable[=VARNAME]", String,
+           "DCL variable (symbol) name for expression result",
+           " (default variable name is #{DEFAULT_DCLSYMBOL},",
+           "  but ignored if not hosted on VMS (OpenVMS))" ) do |val|
+    options[:dclsymbol] = ( val || DEFAULT_DCLSYMBOL ).upcase
+  end  # -r --symbol  --variable
+  opts.separator "#{VMSONLY_BORDEREND}\n\n"
   opts.on( "-n", "--noop", "--dryrun", "--test",
            "Dry-run (test & display, no-op) mode" ) do |val|
     options[:noop]  = true
@@ -89,8 +101,10 @@ optparse = OptionParser.new { |opts|
   end  # -a --about
   # --- Set the banner & Help option ---
   opts.banner = "\n  Usage: #{PROGNAME} [options] \"EXPRESSION\"" +
-                "\n\n    where \"EXPRESSION\" is a numeric expression to" +
-                "\n    evaluate and display in the selected format\n\n"
+                "\n\n    where \"EXPRESSION\" is a numeric expression to evaluate and display in" +
+                "\n    the selected format.  Enclose the expression in double-quotes, e.g." +
+                "\n    \"2**64\" to ensure that special characters such as asterisk/splats are" +
+                "\n    not misinterpreted.\n\n"
   opts.on_tail( "-?", "-h", "--help", "Display this help text" ) do |val|
     $stdout.puts opts
     # $stdout.puts "«+»Additional Text«+»"
@@ -107,6 +121,8 @@ end                           #
 ###############################
 
 options[:os] = WhichOS.identify_os
+
+pp options if options[:debug] >= DBGLVL2
 
 # Strip commas from each arg:
 ARGV.each_with_index { |a,i| ARGV[i] = a.tr( ',', '' ) }
@@ -147,6 +163,7 @@ when :linux
   puts result
 when :vms
   # tuck result into a DCL GLobal Symbol
+  puts result
 end  # case
 
 exit true
