e2560d8c63c3f5df07846cba39ecd53654c38b79
Lorin Ricker <Lorin@RickerNet.us>
2018-05-09 14:51:34 -0600

    fix bug - always restore stty echo on exit or quit commands

--- lib/GetPrompted.rb
+++ lib/GetPrompted.rb
@@ -4,7 +4,7 @@
 # GetPrompted.rb
 #
 # Copyright © 2011-2018 Lorin Ricker <Lorin@RickerNet.us>
-# Version 4.0, 03/27/2018
+# Version 4.1, 05/09/2018
 #
 # This program is free software, under the terms and conditions of the
 # GNU General Public License published by the Free Software Foundation.
@@ -25,11 +25,17 @@ def getprompted( prompt, default, returnexit = false )
   dstr ||= default
   pstr = prompt + ( dstr == "" ? ": " : " [#{dstr}]: " )
   response = readline( pstr, true ).strip
-  exit true if response.downcase == "exit" || response.downcase == "quit"
+  if response.downcase == "exit" || response.downcase == "quit"
+    # Always restore terminal echo:
+    `stty echo`
+    exit true  # this exit always provides cmd-line status:0
+  end
   return ( response != "" ? response : dstr )
 rescue StandardError
   return false if returnexit  # to let caller handle the exit on its own...
   # otherwise:
+  # Always restore terminal echo:
+  `stty echo`
   exit true  # this exit always provides cmd-line status:0
 end #getprompted
 
